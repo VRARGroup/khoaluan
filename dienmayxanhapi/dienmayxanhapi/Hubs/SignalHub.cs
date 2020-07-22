99342de77b1f620d5338d136e7ccb351ad1892d1
vinh123nguyen <49841280+vinh123nguyen@users.noreply.github.com>
2020-07-22 22:40:07 +0700

    fix load lỗi

---
+++ dienmayxanhapi/dienmayxanhapi/Hubs/SignalHub.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace dienmayxanhapi.Hubs
+{
+  public class SignalHub: Hub
+  {
+  }
+}
